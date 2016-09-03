f5d3095b524f678a13f6536de2373cc4f26e6139
Clay Diffrient <cdiffrient@instructure.com>
2016-09-03 07:46:23 -0600

    Add initial work on transaction log

--- index.android.js
+++ index.android.js
@@ -10,12 +10,44 @@ import {
   StyleSheet,
   Text,
   View,
-  TextInput
+  TextInput,
+  ListView
 } from 'react-native';
+
+import {Button} from 'native-base';
 
 import {Column as Col, Row} from 'react-native-flexbox-grid';
 
 class BFTrack extends Component {
+
+  constructor () {
+    super();
+    const ds = new ListView.DataSource({
+      rowHasChanged (r1, r2) {
+        return r1 !== r2;
+      }
+    });
+
+    const initialTransactionData = ['row 1', 'row 2']
+
+    this.state = {
+      transactionData: initialTransactionData,
+      transactionDataSource: ds.cloneWithRows(initialTransactionData)
+    };
+
+    this.addTransaction = this.addTransaction.bind(this);
+  }
+
+  addTransaction () {
+    this.seq = 0
+    const oldData = this.state.transactionData
+    const newData = oldData.concat(['a new row' + this.seq++]);
+    this.setState({
+      transactionData: newData,
+      transactionDataSource: this.state.transactionDataSource.cloneWithRows(newData)
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -73,6 +105,28 @@ class BFTrack extends Component {
         <Row>
           <Col sm={12}>
             <Text>Transaction Log</Text>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={12}>
+            <ListView
+              dataSource={this.state.transactionDataSource}
+              renderHeader={() => (
+                <Row size={8}>
+                  <Col sm={4}><Text>Stock</Text></Col>
+                  <Col sm={2}><Text>Quantity</Text></Col>
+                  <Col sm={2}><Text>Gain/Loss</Text></Col>
+                </Row>
+              )}
+              renderRow={(rowData) => <Text>{rowData}</Text>}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={12}>
+            <Button
+              onPress={this.addTransaction}
+              block>Add Transaction</Button>
           </Col>
         </Row>
       </View>
