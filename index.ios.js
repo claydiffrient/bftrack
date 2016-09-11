5034da40b1610d7a5637fae75dd4a6548ac3bd98
Clay Diffrient <cdiffrient@instructure.com>
2016-09-11 08:47:00 -0600

    Make iOS parity with Android

--- index.ios.js
+++ index.ios.js
@@ -1,44 +1,19 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
+import { AppRegistry } from 'react-native';
+import { Provider } from 'react-redux';
 import SharedIndex from './shared/SharedIndex';
+import configureStore from './shared/store';
+
+const store = configureStore();
 
 class BFTrack extends Component {
   render() {
     return (
-      <SharedIndex />
+      <Provider store={store}>
+        <SharedIndex />
+      </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('BFTrack', () => BFTrack);
